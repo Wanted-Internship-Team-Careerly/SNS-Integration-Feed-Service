3de5856fc6248622195ca7ced7c935a3151a73e2
JisooPyo <audwl326@gmail.com>
2023-10-25 15:14:14 +0900

    init: first commit

---
+++ src/main/java/com/snsIntegrationFeedService/SnsIntegrationFeedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.snsIntegrationFeedService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsIntegrationFeedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsIntegrationFeedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snsIntegrationFeedService/SnsIntegrationFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snsIntegrationFeedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsIntegrationFeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
