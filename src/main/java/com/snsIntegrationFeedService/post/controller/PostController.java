b51f7f3463a2a1ac93b68122df0a5bb6793e0094
9898s <sueh9898@icloud.com>
2023-10-26 21:15:43 +0900

    complete: Post API Tag 추가 완료

--- src/main/java/com/snsIntegrationFeedService/post/controller/PostController.java
+++ src/main/java/com/snsIntegrationFeedService/post/controller/PostController.java
@@ -3,12 +3,14 @@ package com.snsIntegrationFeedService.post.controller;
 import com.snsIntegrationFeedService.post.dto.PostDetailResponseDto;
 import com.snsIntegrationFeedService.post.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Post API", description = "Post 관련 API 정보를 담고 있습니다.")
 @RequiredArgsConstructor
 @RestController
 public class PostController {
