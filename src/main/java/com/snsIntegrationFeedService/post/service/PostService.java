aa70b76b696e4aba0d4e92c22fc2b145891eba9a
9898s <sueh9898@icloud.com>
2023-10-27 08:03:13 +0900

    refactor: static 사용 안하기

--- src/main/java/com/snsIntegrationFeedService/post/service/PostService.java
+++ src/main/java/com/snsIntegrationFeedService/post/service/PostService.java
@@ -1,7 +1,6 @@
 package com.snsIntegrationFeedService.post.service;
 
-import static com.snsIntegrationFeedService.common.error.CustomErrorCode.POST_ID_NOT_FOUND;
-
+import com.snsIntegrationFeedService.common.error.CustomErrorCode;
 import com.snsIntegrationFeedService.common.exception.CustomException;
 import com.snsIntegrationFeedService.post.dto.PostDetailResponseDto;
 import com.snsIntegrationFeedService.post.entity.Post;
@@ -23,7 +22,7 @@ public class PostService {
   public PostDetailResponseDto getPostDetail(String postId) {
     // 예외 처리
     Post post = postRepository.findByPostId(postId).orElseThrow(
-        () -> new CustomException(POST_ID_NOT_FOUND)
+        () -> new CustomException(CustomErrorCode.POST_ID_NOT_FOUND)
     );
 
     // 해시 태그명 가져오기
