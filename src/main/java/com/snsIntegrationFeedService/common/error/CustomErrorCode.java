3b835a0844ec79da357bfcde8ec8dda883eed671
JisooPyo <audwl326@gmail.com>
2023-10-27 16:33:55 +0900

    complete: 회원 가입 API

--- src/main/java/com/snsIntegrationFeedService/common/error/CustomErrorCode.java
+++ src/main/java/com/snsIntegrationFeedService/common/error/CustomErrorCode.java
@@ -7,9 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CustomErrorCode {
-	// 예시 에러코드입니다.
-	USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 사용자입니다."),
 	POST_ID_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 게시글 아이디입니다."),
+	USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 사용자입니다."),
 	;
 
 	private final int errorCode;
--- src/main/java/com/snsIntegrationFeedService/user/controller/UserController.java
+++ src/main/java/com/snsIntegrationFeedService/user/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.snsIntegrationFeedService.user.controller;
 
+import com.snsIntegrationFeedService.common.dto.ApiResponseDto;
+import com.snsIntegrationFeedService.user.dto.SignupRequestDto;
+import com.snsIntegrationFeedService.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@Tag(name = "사용자 API", description = "사용자와 관련된 API 정보를 담고 있습니다.")
 public class UserController {
+	private final UserService userService;
+
+	@Operation(summary = "회원 가입", description = "가입에 필요한 정보를 받아 회원가입합니다.")
+	@PostMapping("/signup")
+	public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) {
+		userService.signup(requestDto);
+		return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "회원 가입 완료"));
+	}
+
+
 }
