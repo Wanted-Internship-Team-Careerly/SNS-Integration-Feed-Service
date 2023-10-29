67b5df83f7f88389c4d09df9bdc7333c5178d8f9
JisooPyo <audwl326@gmail.com>
2023-10-29 15:54:45 +0900

    [feature] 회원가입 API (#14)
    * chore: 테스트 편의 위해 Security 기능 off
    
    나중에 Security 구현 시 다시 Security on
    
    * refactor: `@Builder` 어노테이션 추가 및 isAccessed 필드에 ``@Builder.Default` 걸어주기
    
    * complete: 회원가입에 필요한 SignupRequestDto 생성
    
    * complete: 회원 가입 API
    
    * complete: 회원가입시 비밀번호 검증 로직 구현
    
    * fix: 객체 Bean 등록
    
    객체 선언만 해주어서 nullPointerException 발생, Bean으로 등록하여 관리
    
    * refactor: 회원가입 성공 상태 코드 CREATED로 변경
    
    * refactor: 유저 정보 - 전역변수 -> 매개변수

--- src/main/java/com/snsIntegrationFeedService/user/service/UserService.java
+++ src/main/java/com/snsIntegrationFeedService/user/service/UserService.java
@@ -1,4 +1,47 @@
 package com.snsIntegrationFeedService.user.service;
 
+import com.snsIntegrationFeedService.common.error.CustomErrorCode;
+import com.snsIntegrationFeedService.common.exception.CustomException;
+import com.snsIntegrationFeedService.user.dto.SignupRequestDto;
+import com.snsIntegrationFeedService.user.entity.User;
+import com.snsIntegrationFeedService.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final PasswordValidation passwordValidation;
+
+	public void signup(SignupRequestDto requestDto) {
+		String account = requestDto.getAccount();
+		String email = requestDto.getEmail();
+		String password = requestDto.getPassword();
+
+		// account로 검증하여 회원이 존재하면 예외 발생
+		User targetUser = findUser(account);
+		if (targetUser != null) {
+			throw new CustomException(CustomErrorCode.USER_ALREADY_EXIST);
+		}
+
+		// 비밀번호 검증이 완료되면 password encoding
+		passwordValidation.validatePassword(requestDto);
+		password = passwordEncoder.encode(password);
+
+		User user = User.builder()
+				.account(account).email(email)
+				.password(password).build();
+		userRepository.save(user);
+	}
+
+	private User findUser(String account) {
+		return userRepository.findByAccount(account).orElse(null);
+	}
+
 }
