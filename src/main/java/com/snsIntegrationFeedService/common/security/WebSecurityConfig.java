5314199d3814c6762aaa9b6e105dbc99214730ab
JisooPyo <audwl326@gmail.com>
2023-10-31 01:04:42 +0900

    refactor: 가입승인 API 요청 접근 허가

--- src/main/java/com/snsIntegrationFeedService/common/security/WebSecurityConfig.java
+++ src/main/java/com/snsIntegrationFeedService/common/security/WebSecurityConfig.java
@@ -53,6 +53,7 @@ public class WebSecurityConfig {
 				authorizeHttpRequests
 						.requestMatchers(HttpMethod.POST, "/api/users/**").permitAll() // '/api/users'로 시작하는 요청 중 모든 POST 접근 허가
 						.requestMatchers("/swagger-ui/**", "/v3/**").permitAll() // swagger-ui 와 관련된 모든 요청 접근 허가
+						.requestMatchers(HttpMethod.POST, "/api/verifications").permitAll() // 가입승인 API 요청 허가
 						.anyRequest().authenticated() // 그 외 모든 요청 인증처리
 		);
 
