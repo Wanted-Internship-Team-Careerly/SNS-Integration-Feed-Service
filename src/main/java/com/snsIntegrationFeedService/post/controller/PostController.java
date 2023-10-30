d4e5b47ada4b398b14e5452c4520a3507b20c9a3
suhwan <sueh9898@icloud.com>
2023-10-31 00:15:04 +0900

    [feature] 게시물 목록 API (#15)
    * build: querydsl 의존성 추가
    
    * feat: 직렬화 하지 않도록 설정
    
    * complete: querydsl 설정 완료
    
    * complete: 게시글 검색 구현 완료
    
    * update: 해쉬태그 빈값일 시 계정으로 조회
    
    * refactor: entity manager를 빈으로 등록
    
    * refactor: account 값만 넘기는 방식으로 수정
    
    * refactor: 직렬화 설정한 어노테이션 제거
    
    * refactor: Q클래스 수정
    
    * refactor: import 정리
    
    * refactor: 컨트롤러에서의 userDetails 전달 방식 변경

--- src/main/java/com/snsIntegrationFeedService/post/controller/PostController.java
+++ src/main/java/com/snsIntegrationFeedService/post/controller/PostController.java
@@ -1,11 +1,15 @@
 package com.snsIntegrationFeedService.post.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.snsIntegrationFeedService.common.security.UserDetailsImpl;
 import com.snsIntegrationFeedService.post.dto.PostDetailResponseDto;
+import com.snsIntegrationFeedService.post.dto.PostsResponseDto;
 import com.snsIntegrationFeedService.post.service.PostService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,5 +28,23 @@ public class PostController {
 	public ResponseEntity<PostDetailResponseDto> getPostDetail(@PathVariable String postId) {
 		PostDetailResponseDto postDetailResponseDto = postService.getPostDetail(postId);
 		return ResponseEntity.ok().body(postDetailResponseDto);
+	}
+
+	@Operation(summary = "게시글 목록", description = "유저가 검색한 게시글 목록을 보는 API")
+	@GetMapping("/api/posts")
+	public ResponseEntity<?> getPosts(
+		@RequestParam(required = false) String hashtag,
+		@RequestParam(required = false) String type,
+		@RequestParam(name = "order_by", defaultValue = "created_at", required = false) String orderBy,
+		@RequestParam(name = "sort_by", defaultValue = "desc", required = false) String sortBy,
+		@RequestParam(name = "search_by", required = false) String searchBy,
+		@RequestParam(required = false) String search,
+		@RequestParam(name = "page_count", defaultValue = "10", required = false) int pageCount,
+		@RequestParam(name = "page", defaultValue = "0", required = false) int page,
+		@AuthenticationPrincipal UserDetailsImpl userDetails
+	) {
+		PostsResponseDto posts =
+			postService.getPosts(hashtag, type, orderBy, sortBy, searchBy, search, pageCount, page, userDetails);
+		return ResponseEntity.ok().body(posts);
 	}
 }
