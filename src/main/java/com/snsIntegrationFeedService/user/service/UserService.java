3be1098eed369e0cd8fdf1685a76bc7189ed2bad
jonghun <jonghuncu@gmail.com>
2023-11-03 17:11:23 +0900

    fix : private 변경

--- src/main/java/com/snsIntegrationFeedService/user/service/UserService.java
+++ src/main/java/com/snsIntegrationFeedService/user/service/UserService.java
@@ -40,7 +40,7 @@ public class UserService {
 		userRepository.save(user);
 	}
 
-	public User findUser(String account) {
+	private User findUser(String account) {
 		return userRepository.findByAccount(account).orElse(null);
 	}
 
