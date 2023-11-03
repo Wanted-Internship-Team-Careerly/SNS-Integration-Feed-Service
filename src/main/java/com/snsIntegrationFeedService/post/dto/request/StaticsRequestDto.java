7c3969e0262dae71cf28fdfdaf152ce4b05634cd
jonghun <jonghuncu@gmail.com>
2023-11-03 17:11:42 +0900

    rename : dto 이름 추가

--- src/main/java/com/snsIntegrationFeedService/post/dto/request/StaticsRequestDto.java
+++ src/main/java/com/snsIntegrationFeedService/post/dto/request/StaticsRequestDto.java
@@ -3,13 +3,11 @@ package com.snsIntegrationFeedService.post.dto.request;
 import com.snsIntegrationFeedService.post.CountType;
 import com.snsIntegrationFeedService.post.DateType;
 import java.util.Date;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
-public class StaticsRequest {
+public class StaticsRequestDto {
     private CountType value;
     private DateType type;
     private String hashtag;
@@ -20,7 +18,7 @@ public class StaticsRequest {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date end;
 
-    public StaticsRequest(CountType value, DateType type, String hashtag, Date start, Date end) {
+    public StaticsRequestDto(CountType value, DateType type, String hashtag, Date start, Date end) {
         this.value = (value != null) ? value : CountType.count;
         this.type = type;
         this.hashtag = hashtag;
